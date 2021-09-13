4d57a89d8e8b365f82c1b87d76893765197f663f
md.asif.8393 <md.asif.8393@gmail.com>
2021-09-13 23:23:24 +0600

    practise

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.get('/',  (req, res) =>{
-  res.send('I know how to open node');
-})
+const users = ['Abid', 'Kabid', 'Labid', 'Jamid', 'Hammid', 'Labib', 'Samad'];
+
+app.get('/', (req, res) => {
+  const fruit = {
+    product: 'ada',
+    price: 220
+    
+  }
+  res.send(fruit);
+});
+
+app.get('/fruits/banana', (req, res) => {
+  res.send({fruit: "banana", quantity: 1000, price: 100000});
+});
+
+
 
 app.listen(3000 , () => console.log('listening on port 3000'));
